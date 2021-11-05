1ca70bbc5a0885569586aebbc00d97431a6b0d78
a01022161 <91310034+a01022161@users.noreply.github.com>
2021-11-05 15:55:14 +0800

    Update 3.login.php

--- 3.login.php
+++ 3.login.php
@@ -1,5 +1,5 @@
 <?php 
-    if (($_POST[id] == "john") && ($_POST[pwd]=="john1234"))  #如果 $_POST[id] 的值等於john and  $_POST[pwd]的值等於john1234
+    if (($_POST[id] == "john") && ($_POST[pwd]=="john1234"))  #如果 $_POST[id] 的值等於john and  $_POST[pwd]的值等於john1234 則為true
         echo "Welcome";   #輸出 Welcome
     else #否則
         echo "fail login"; #輸出 fail login
