33498399a585564bd13799dfd77ffab6077d96e6
a01022161 <91310034+a01022161@users.noreply.github.com>
2021-11-05 13:01:59 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,11 @@
+
+<html>
+    <head><title>html+php</title></head>   #標題  
+    <body>
+        <?php echo "Hello world!"; ?> #使用php來輸出Hello world!
+        <h1>html</h1> #大寫並換行
+        <?php echo "Hello world!"; ?> #使用php來輸出Hello world!
+        <h1>html</h1> #大寫並換行
+        <?php echo "Hello world!"; ?> #使用php來輸出Hello world!
+    </body>
+</html>
